76fe0ed986b9d6d10c7bba42d644507d7d1b707b
smg-444 <sessamy444@gmail.com>
2025-11-24 00:44:17 +0100

    Delete app directory

--- app/src/main/java/es/etg/lectoguard/domain/usecase/GetBooksUseCase.kt
+++
@@ -1,7 +0,0 @@
-package es.etg.lectoguard.domain.usecase
-
-import es.etg.lectoguard.data.repository.BookRepository
-
-class GetBooksUseCase(private val repository: BookRepository) {
-    suspend operator fun invoke(isOnline: Boolean) = repository.getAllBooks(isOnline)
-} 
--- app/src/main/java/es/etg/lectoguard/domain/usecase/LoginUseCase.kt
+++
@@ -1,7 +0,0 @@
-package es.etg.lectoguard.domain.usecase
-
-import es.etg.lectoguard.data.repository.UserRepository
- 
-class LoginUseCase(private val repository: UserRepository) {
-    suspend operator fun invoke(email: String, password: String) = repository.login(email, password)
-} 
